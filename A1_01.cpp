a9f6a6a3ac667bdd27271bf5d87c03843edbeec2
pardparw <106542860+thanabest08@users.noreply.github.com>
2025-04-02 16:31:17 +0700

    finish

---
+++ A1_01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string a,a2;
+	cin >> a >> a2;
+	cout << "Hello "+a+" "+a2 + "\n";
+	cout << a[0];
+	cout << a[1];
+	cout << a2[0];
+	cout << a2[1];
+}
