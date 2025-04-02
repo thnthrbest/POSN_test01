8ab34b8119b8f89db1635f1a9317c3568c9b63f3
pardparw <106542860+thanabest08@users.noreply.github.com>
2025-04-02 16:26:18 +0700

    A1_02

--- test.cpp
+++ test.cpp
@@ -3,11 +3,14 @@
 using namespace std;
 
 int main(){
-	string a,a2;
-	cin >> a >> a2;
-	cout << "Hello "+a+" "+a2 + "\n";
-	cout << a[0];
-	cout << a[1];
-	cout << a2[0];
-	cout << a2[1];
+	int a;
+	cin >> a;
+	cout <<"10 = "<< (int)a/10 << "\n";
+	a-=((a/10)*10);
+	cout <<"5 = "<< (int)a/5 << "\n";
+	a-=((a/5)*5);
+	cout <<"2 = "<< (int)a/2 << "\n";
+	a-=((a/2)*2);
+	cout <<"1 = "<< (int)a/1 << "\n";
+	a-=((a/1)*1);
 }
