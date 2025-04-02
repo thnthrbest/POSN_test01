fd14460e00895ce64ac1a78fe205922ff1db333e
pardparw <106542860+thanabest08@users.noreply.github.com>
2025-04-02 19:51:45 +0700

    tt

--- A1_01.cpp
+++ A1_01.cpp
@@ -5,9 +5,5 @@ using namespace std;
 int main(){
 	string a,a2;
 	cin >> a >> a2;
-	cout << "Hello "+a+" "+a2 + "\n";
-	cout << a[0];
-	cout << a[1];
-	cout << a2[0];
-	cout << a2[1];
+	cout << "Hello "<< a <<" "+a2 << "\n" << a[0] << a[1] << a2[0] << a2[1];
 }
---
+++ A1_06.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin >> a >> b;
+    if(a%b==0)cout << "yes";
+    else cout << "no";
+}
