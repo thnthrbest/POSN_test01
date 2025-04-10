3a5f59d7149f14caf4a48dc13f24b6bc0ae75389
pardparw <106542860+thanabest08@users.noreply.github.com>
2025-04-10 11:24:50 +0700

    f

--- A1_11.cpp
+++ A1_11.cpp
@@ -5,10 +5,10 @@ int main(){
     int temp=0,count=0;
     cin >> a;
     for(int i=0;i<a.length();i++){
-			for(;a[i]==a[i+temp];temp++);
-        	if(a[i]!=b[0])cout << temp << a[i];
-        	b =a[i];
-        	temp=0;
+		for(;a[i]==a[i+temp];temp++);
+        if(a[i]!=b[0])cout << temp << a[i];
+        b =a[i];
+        temp=0;
     }
 
 }
